12915a8542fbd7642961e75cdb900a95c07249be
Jcc20 <48126929+Jcc20@users.noreply.github.com>
2021-01-29 16:40:53 +0000

    Update HR.sql

--- TP/HR.sql
+++ TP/HR.sql
@@ -1,4 +1,4 @@
-/* (ID=101) todos os empregados com quem um certo empregado ja trabalhou */ EXEC TIME = 0.288
+/* (ID=101) todos os empregados com quem um certo empregado ja trabalhou */ EXEC TIME = 288ms
 with depAtual AS (SELECT department_id, hire_date from Employees where employee_id=101),
     depsAntigos AS (SELECT department_id, start_date, end_date from Job_history where employee_id=101)
 select distinct(e.employee_id), concat(concat(e.first_name,' '), e.last_name) as Nome
@@ -12,7 +12,7 @@ or (jh.employee_id = e.employee_id and jh.department_id = dAnt.department_id and
     (jh.start_date < dAnt.start_date and jh.end_date > dAnt.end_date)))
 order by employee_id;
 
-/* top 5 empregados mais antigos em cada regiao */ EXEC TIME = 0.076
+/* top 5 empregados mais antigos em cada regiao */ EXEC TIME = 76ms
 select Regiao, Nome, Data_Inicio
 from (select r.region_name as Regiao, concat(concat(e.first_name,' '), e.last_name) as Nome, e.hire_date as Data_Inicio,
       rank() over (partition by r.region_name order by e.hire_date asc, concat(concat(e.first_name,' '), e.last_name) asc) rank
@@ -24,7 +24,7 @@ from (select r.region_name as Regiao, concat(concat(e.first_name,' '), e.last_na
      )
 where rank <= 5;
 
-/* (ID = 176) trabalho em que um user (com varios trabalhos no historico) faturou mais */ EXEC TIME = 0.012
+/* (ID = 176) trabalho em que um user (com varios trabalhos no historico) faturou mais */ EXEC TIME = 12ms
 select j.JOB_TITLE as Emprego, jh.start_date as DataInicial, jh.END_DATE  as DataFinal, Round((MONTHS_BETWEEN (jh.end_date,jh.start_date))*j.MAX_SALARY,2) as Faturado from EMPLOYEES e
 inner join JOB_HISTORY jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID 
 inner join JOBS j on jh.JOB_ID = j.JOB_ID
@@ -33,13 +33,13 @@ where e.EMPLOYEE_ID = 176
 order by (MONTHS_BETWEEN (jh.end_date,jh.start_date))*j.MAX_SALARY desc
 fetch first 1 rows only;
 
-/* Media de salarios por departamento */ EXEC TIME = 0.035
+/* Media de salarios por departamento */ EXEC TIME = 35ms
 select d.department_name as Departamento, round(avg(e.salary),2) as Media_Salario from employees e
 inner join departments d on e.department_id=d.department_id
 group by d.department_name
 order by Media_Salario desc;
 
-/* Total de empregadaos que trabalharam em cada departamento */ EXEC TIME = 0.027
+/* Total de empregadaos que trabalharam em cada departamento */ EXEC TIME = 27ms
 with employees_by_dep as (
     select d.department_id, jh.employee_id from Departments d, Job_history jh where jh.department_id = d.department_id
     union all
